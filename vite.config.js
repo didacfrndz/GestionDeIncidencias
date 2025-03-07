d0f21e290f9286fd201ca0b3191d8b16779fbdb1
didacfrndz <156692497+didacfrndz@users.noreply.github.com>
2025-03-07 16:03:27 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  root: "./src",   // Indicamos que la raiz de nuestro proyecto es la carpeta src
-  base: "./", // Indicamos que use el . para indicar la carpeta actual
-  publicDir: "../public", // Indicamos donde tendremos los archivos públicos, como imágenes, etc
-  build: {
-    outDir: "../dist", // Indicamos donde queremos que se cree la carpeta de distribución 
-    assetsDir: "assets"
-  },
-  plugins: []
-});
+  plugins: [react()],
+})
